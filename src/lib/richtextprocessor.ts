b4b7a4cc9964f86cfeff13580ec5dccc8d2fd630
Eduard Kuzmenko <thanwords24@gmail.com>
2020-11-16 04:23:37 +0200

    Returned [contenteditable] on emoji for input font

--- src/lib/richtextprocessor.ts
+++ src/lib/richtextprocessor.ts
@@ -512,8 +512,8 @@ namespace RichTextProcessor {
           break;
 
         case 'messageEntityEmoji':
-          html.push(emojiSupported ? // ! contenteditable="false" было у span, зачем?
-            `<span class="emoji">${encodeEntities(entityText)}</span>` : 
+          html.push(emojiSupported ? // ! contenteditable="false" нужен для поля ввода, иначе там будет меняться шрифт в Safari
+            `<span contenteditable="false" class="emoji">${encodeEntities(entityText)}</span>` : 
             `<img src="assets/img/emoji/${entity.unicode}.png" alt="${encodeEntities(entityText)}" class="emoji">`);
           break;
 
