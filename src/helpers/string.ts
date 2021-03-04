ab645ffb4d60b025ad04934065dcc1937535e08f
morethanwords <thanwords24@gmail.com>
2021-03-04 22:52:46 +0400

    Fix deadlock limitSymbols

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -3,8 +3,9 @@
 } */
 
 export function limitSymbols(str: string, length: number, limitFrom = length + 10) {
+  str = str.trim();
   if(str.length > limitFrom) {
-    str = str.slice(0, length).replace(/(\n|\s)+$/, '') + '...';
+    str = str.slice(0, length)/* .replace(/\s*$/, '') */ + '...';
   }
 
   return str;
