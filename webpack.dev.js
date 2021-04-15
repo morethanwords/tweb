17f87f2e3716b0b4ad9d65be2640f7e768fd4603
Eduard Kuzmenko <thanwords24@gmail.com>
2021-04-15 12:44:43 +0400

    Ignore chunks on dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
 process.env.NODE_ENV = 'development';
 
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ],
 });
