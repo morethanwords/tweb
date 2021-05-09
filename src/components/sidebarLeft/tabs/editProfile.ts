7a1f2a3c5bc50dbabe1ac6623b7cc98cff43b9d7
Eduard Kuzmenko <thanwords24@gmail.com>
2021-05-09 21:02:35 +0400

    Fix clearing username

--- src/components/sidebarLeft/tabs/editProfile.ts
+++ src/components/sidebarLeft/tabs/editProfile.ts
@@ -135,7 +135,7 @@ export default class AppEditProfileTab extends SliderSuperTab {
         }));
       }
 
-      if(this.usernameInputField.isValid() && this.usernameInputField.input.classList.contains('valid')) {
+      if(this.usernameInputField.isValid() && !this.usernameInputField.input.classList.contains('error')) {
         promises.push(appUsersManager.updateUsername(this.usernameInputField.value));
       }
 
