6451e964a884009abb3a814076546fef1492fd3e
morethanwords <thanwords24@gmail.com>
2021-06-18 13:53:09 +0300

    Build

---
+++ public/22.aeec566414c07891928e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[22],{19:function(n,o,t){"use strict";t.r(o),t.d(o,"isTouchSupported",(function(){return c}));const c="ontouchstart"in window||window.DocumentTouch&&document instanceof DocumentTouch}}]);
+//# sourceMappingURL=22.aeec566414c07891928e.chunk.js.map
