bcb7427569afb5d021be2b275aa534e19e642d3f
Eduard Kuzmenko <thanwords24@gmail.com>
2021-07-08 09:07:53 +0300

    Change version

--- src/config/app.ts
+++ src/config/app.ts
@@ -16,7 +16,7 @@ export const MAIN_DOMAIN = 'web.telegram.org';
 const App = {
   id: 1025907,
   hash: '452b0359b988148995f22ff0f4229750',
-  version: '0.5.9',
+  version: '0.6.0',
   langPackVersion: '0.3.0',
   langPack: 'macos',
   langPackCode: 'en',
