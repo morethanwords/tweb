686bbe17fc8190448b385cb056728300c0c45154
morethanwords <thanwords24@gmail.com>
2021-08-03 05:05:59 +0300

    messed up repo

--- src/components/popups/sendNow.ts
+++ src/components/popups/sendNow.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * https://github.com/morethanwords/tweb
  * Copyright (C) 2019-2021 Eduard Kuzmenko
@@ -35,42 +34,4 @@ export default class PopupSendNow {
 
     popup.show();
   }
-=======
-/*
- * https://github.com/morethanwords/tweb
- * Copyright (C) 2019-2021 Eduard Kuzmenko
- * https://github.com/morethanwords/tweb/blob/master/LICENSE
- */
-
-import appMessagesManager from "../../lib/appManagers/appMessagesManager";
-import { PopupButton } from ".";
-import PopupPeer from "./peer";
-
-export default class PopupSendNow {
-  constructor(peerId: number, mids: number[], onConfirm?: () => void) {
-    let title: string, description: string, buttons: PopupButton[] = [];
-
-    title = `Send Message${mids.length > 1 ? 's' : ''} Now`;
-    description = mids.length > 1 ? 'Send ' + mids.length + ' messages now?' : 'Send message now?';
-
-    const callback = () => {
-      onConfirm && onConfirm();
-      appMessagesManager.sendScheduledMessages(peerId, mids);
-    };
-
-    buttons.push({
-      langKey: 'Send',
-      callback
-    });
-
-    const popup = new PopupPeer('popup-delete-chat', {
-      peerId,
-      title,
-      description,
-      buttons
-    });
-
-    popup.show();
-  }
->>>>>>> a3a258651320e5e8e7903d8bfe1cb222d84de6dc
 }
