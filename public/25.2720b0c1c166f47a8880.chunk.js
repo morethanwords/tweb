865d553a4a0f005a1c9ea8de7ad2caaf709b06a9
Eduard Kuzmenko <thanwords24@gmail.com>
2021-08-13 22:26:25 +0300

    Build

--- public/25.2720b0c1c166f47a8880.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[25],{15:function(n,o,t){"use strict";t.r(o),t.d(o,"isTouchSupported",(function(){return c}));const c="ontouchstart"in window||window.DocumentTouch&&document instanceof DocumentTouch}}]);
-//# sourceMappingURL=25.2720b0c1c166f47a8880.chunk.js.map
