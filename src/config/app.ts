438525f97af848e2bf5303d884aad0c607b3f82f
morethanwords <thanwords24@gmail.com>
2021-09-15 09:03:35 +0400

    Build

--- src/config/app.ts
+++ src/config/app.ts
@@ -16,7 +16,7 @@ export const MAIN_DOMAIN = 'web.telegram.org';
 const App = {
   id: 1025907,
   hash: '452b0359b988148995f22ff0f4229750',
-  version: '0.8.3',
+  version: '0.8.4',
   langPackVersion: '0.3.3',
   langPack: 'macos',
   langPackCode: 'en',
