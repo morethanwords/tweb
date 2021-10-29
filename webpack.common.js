5bd76a76eb8abf1abeae628f8f46fb43638dab1f
morethanwords <thanwords24@gmail.com>
2021-10-29 23:10:17 +0300

    webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ const Dotenv = require('dotenv-webpack');
 const allowedIPs = ['127.0.0.1'];
 const devMode = process.env.NODE_ENV !== 'production';
 const useLocal = true;
-const useLocalNotLocal = true;
+const useLocalNotLocal = false;
 
 if(devMode) {
   console.log('DEVMODE IS ON!');
