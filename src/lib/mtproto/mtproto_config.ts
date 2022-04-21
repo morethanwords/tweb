68aec67f5e4bfd69a33bab3331b49cb60888e16b
Eduard Kuzmenko <thanwords24@gmail.com>
2022-04-21 19:35:23 +0300

    Fix displaying broken comments

--- src/lib/mtproto/mtproto_config.ts
+++ src/lib/mtproto/mtproto_config.ts
@@ -12,6 +12,7 @@ export type UserAuth = {dcID: number | string, date: number, id: PeerId};
 
 export const NULL_PEER_ID: PeerId = 0;
 export const REPLIES_PEER_ID: PeerId = 1271266957;
+export const REPLIES_HIDDEN_PEER_ID: PeerId = 777;
 export const SERVICE_PEER_ID: PeerId = 777000;
 export const MUTE_UNTIL = 0x7FFFFFFF;
 export const BOT_START_PARAM = '';
