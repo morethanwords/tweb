2f091a98f30b446878722a8032dde573db61f31e
Eduard Kuzmenko <thanwords24@gmail.com>
2022-04-26 00:08:39 +0300

    Fix getting html from inputs

--- src/helpers/dom/documentFragmentToHTML.ts
+++ src/helpers/dom/documentFragmentToHTML.ts
@@ -1,5 +1,7 @@
+import encodeEntities from "../string/encodeEntities";
+
 export default function documentFragmentToHTML(fragment: DocumentFragment) {
   return Array.from(fragment.childNodes).map((node) => {
-    return node.nodeType === 3 ? node.textContent : (node as Element).outerHTML;
+    return node.nodeType === 3 ? encodeEntities(node.textContent) : (node as Element).outerHTML;
   }).join('');
 }
