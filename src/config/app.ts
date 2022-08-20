bd40320c7f863ae76ba9809ca295f4cb02ab14aa
Eduard Kuzmenko <thanwords24@gmail.com>
2022-08-20 14:00:52 +0200

    Reset langpacks

--- src/config/app.ts
+++ src/config/app.ts
@@ -19,7 +19,7 @@ const App = {
   version: process.env.VERSION,
   versionFull: process.env.VERSION_FULL,
   build: +process.env.BUILD,
-  langPackVersion: '0.4.4',
+  langPackVersion: '0.4.5',
   langPack: 'macos',
   langPackCode: 'en',
   domains: [MAIN_DOMAIN] as string[],
