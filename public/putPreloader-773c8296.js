0888fc380b854eb1b934257978748949c98fc13a
Eduard Kuzmenko <thanwords24@gmail.com>
2023-09-18 22:04:38 +0400

    Build

--- public/putPreloader-773c8296.js
+++
@@ -1,5 +0,0 @@
-import{M as i,al as o}from"./index-3450ac1f.js";function a(r,n=!1){const e=`
-  <svg xmlns="http://www.w3.org/2000/svg" class="preloader-circular" viewBox="25 25 50 50">
-  <circle class="preloader-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"/>
-  </svg>`;if(n){const t=document.createElement("div");return t.classList.add("preloader"),t.innerHTML=e,r&&r.appendChild(t),t}return r.insertAdjacentHTML("beforeend",e),r.lastElementChild}i.putPreloader=a;function c(r,n="check"){const e=r.querySelector(`.${o(n)}`);return e?.remove(),r.disabled=!0,a(r),()=>{r.replaceChildren(),e&&r.append(e),r.removeAttribute("disabled")}}export{a as p,c as s};
-//# sourceMappingURL=putPreloader-773c8296.js.map
