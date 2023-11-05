9924d6dbba91594b6b17971dbf2599b7d664207d
Eduard Kuzmenko <thanwords24@gmail.com>
2023-11-06 03:39:00 +0400

    Build

--- public/toggleDisability-6f5940d7.js
+++
@@ -1,2 +0,0 @@
-function a(r,t){return Array.isArray(r)||(r=[r]),t?r.forEach(i=>i.setAttribute("disabled","true")):r.forEach(i=>i.removeAttribute("disabled")),()=>a(r,!t)}export{a as t};
-//# sourceMappingURL=toggleDisability-6f5940d7.js.map
