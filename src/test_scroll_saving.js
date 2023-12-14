10769e048be0383b1ea644a19e3aebd89fdd0cb8
Eduard Kuzmenko <thanwords24@gmail.com>
2023-12-14 23:15:29 +0400

    Fix extra variables in global scope

--- src/test_scroll_saving.js
+++ src/test_scroll_saving.js
@@ -1,16 +1,18 @@
-var chatInner = appImManager.chat.bubbles.chatInner;
-var dateGroup = chatInner.firstElementChild;
-var topBubble = chatInner.querySelector('[data-mid="6318129151"]').parentElement;
-var bottomBubble = chatInner.querySelector('[data-mid="6318587903"]').parentElement;
-topBubble.remove();
-bottomBubble.remove();
-var f = () => {
-    var scrollSaver = appImManager.chat.bubbles.createScrollSaver();
+{
+  const chatInner = appImManager.chat.bubbles.chatInner;
+  const dateGroup = chatInner.firstElementChild;
+  const topBubble = chatInner.querySelector('[data-mid="6318129151"]').parentElement;
+  const bottomBubble = chatInner.querySelector('[data-mid="6318587903"]').parentElement;
+  topBubble.remove();
+  bottomBubble.remove();
+  const f = () => {
+    const scrollSaver = appImManager.chat.bubbles.createScrollSaver();
     scrollSaver.save();
-    
+
     dateGroup.prepend(topBubble);
     dateGroup.append(bottomBubble);
     scrollSaver.restore();
-};
-// f();
-setTimeout(() => f(), 1000);
+  };
+  // f();
+  setTimeout(() => f(), 1000);
+}
