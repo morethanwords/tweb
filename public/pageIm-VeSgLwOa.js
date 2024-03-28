1caa0bba9660c6f1811e39f47ce661d280e4edb9
Eduard Kuzmenko <thanwords24@gmail.com>
2024-03-28 16:36:15 +0400

    Build

---
+++ public/pageIm-VeSgLwOa.js
@@ -0,0 +1,8 @@
+import{a as o,f as t,h as r,_ as a,l as s}from"./index--yeSbA7G.js";import{P as l}from"./page-RAzEx1J_.js";const n=()=>(o.managers.appStateManager.pushToState("authState",{_:"authStateSignedIn"}),t.requestedServerLanguage||t.getCacheLangPack().then(e=>{e.local&&t.getLangPack(e.lang_code)}),i.pageEl.style.display="",r(),Promise.all([a(()=>import("./appDialogsManager-Z7Ldbp_a.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]),import.meta.url),s(),"requestVideoFrameCallback"in HTMLVideoElement.prototype?Promise.resolve():a(()=>import("./requestVideoFrameCallbackPolyfill-GsYXQx88.js"),__vite__mapDeps([]),import.meta.url)]).then(([e])=>{e.default.start(),setTimeout(()=>{document.getElementById("auth-pages").remove()},1e3)})),i=new l("page-chats",!1,n);export{i as default};
+//# sourceMappingURL=pageIm-VeSgLwOa.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./appDialogsManager-Z7Ldbp_a.js","./avatar-WcVLte-C.js","./button-YT5EoKtN.js","./index--yeSbA7G.js","./index-TgDMmGqu.css","./page-RAzEx1J_.js","./wrapEmojiText-6NosUbRJ.js","./scrollable-c3j4eb9G.js","./putPreloader-M5E9UtjA.js","./htmlToSpan-9hKUY11k.js","./countryInputField-oAvzxUWO.js","./textToSvgURL-Z4O-nL1S.js","./fastBlur-iN9VGk7U.js","./codeInputField-h9MaGGB3.js","./appDialogsManager-ZAqcM05i.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
