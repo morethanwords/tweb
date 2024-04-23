9070845f58b491e6c13b25158238b98489b3f607
Eduard Kuzmenko <thanwords24@gmail.com>
2024-04-23 19:29:09 +0400

    Fix album items border-radius

--- src/components/prepareAlbum.ts
+++ src/components/prepareAlbum.ts
@@ -45,19 +45,19 @@ export default function prepareAlbum(options: {
     div.style.left = (geometry.x / width * 100) + '%';
 
     if(sides & RectPart.Left && sides & RectPart.Top) {
-      div.style.borderTopLeftRadius = 'inherit';
+      div.style.borderStartStartRadius = `calc(var(--border-start-start-radius) - ${options.spacing}px)`;
     }
 
     if(sides & RectPart.Left && sides & RectPart.Bottom) {
-      div.style.borderBottomLeftRadius = 'inherit';
+      div.style.borderEndStartRadius =  `calc(var(--border-end-start-radius) - ${options.spacing}px)`;
     }
 
     if(sides & RectPart.Right && sides & RectPart.Top) {
-      div.style.borderTopRightRadius = 'inherit';
+      div.style.borderStartEndRadius =  `calc(var(--border-start-end-radius) - ${options.spacing}px)`;
     }
 
     if(sides & RectPart.Right && sides & RectPart.Bottom) {
-      div.style.borderBottomRightRadius = 'inherit';
+      div.style.borderEndEndRadius =  `calc(var(--border-end-end-radius) - ${options.spacing}px)`;
     }
 
     if(options.forMedia) {
