5c19c13e7b995fd03600d53c72825b738a9f985e
yzqzss <yzqzss@yandex.com>
2024-06-16 16:37:08 +0800

    Make spoiler not unicode based
    #328

--- src/lib/richTextProcessor/spoiler.ts
+++ src/lib/richTextProcessor/spoiler.ts
@@ -1,11 +1,10 @@
 export default function spoiler(text: string): string {
-  const chars = '⠁⠂⠄⠈⠐⠠⡀⢀⠃⠅⠆⠉⠊⠌⠑⠒⠔⠘⠡⠢⠤⠨⠰⡁⡂⡄⡈⡐⡠⢁⢂⢄⢈⢐⢠⣀⠇⠋⠍⠎⠓⠕⠖⠙⠚⠜⠣⠥⠦⠩⠪⠬⠱⠲⠴⠸⡃⡅⡆⡉⡊⡌⡑⡒⡔⡘⡡⡢⡤⡨⡰⢃⢅⢆⢉⢊⢌⢑⢒⢔⢘⢡⢢⢤⢨⢰⣁⣂⣄⣈⣐⣠⠏⠗⠛⠝⠞⠧⠫⠭⠮⠳⠵⠶⠹⠺⠼⡇⡋⡍⡎⡓⡕⡖⡙⡚⡜⡣⡥⡦⡩⡪⡬⡱⡲⡴⡸⢇⢋⢍⢎⢓⢕⢖⢙⢚⢜⢣⢥⢦⢩⢪⢬⢱⢲⢴⢸⣃⣅⣆⣉⣊⣌⣑⣒⣔⣘⣡⣢⣤⣨⣰⠟⠯⠷⠻⠽⠾⡏⡗⡛⡝⡞⡧⡫⡭⡮⡳⡵⡶⡹⡺⡼⢏⢗⢛⢝⢞⢧⢫⢭⢮⢳⢵⢶⢹⢺⢼⣇⣋⣍⣎⣓⣕⣖⣙⣚⣜⣣⣥⣦⣩⣪⣬⣱⣲⣴⣸⠿⡟⡯⡷⡻⡽⡾⢟⢯⢷⢻⢽⢾⣏⣗⣛⣝⣞⣧⣫⣭⣮⣳⣵⣶⣹⣺⣼⡿⢿⣟⣯⣷⣻⣽⣾⣿';
+  const chars = '⡚⡎⢪⡁⢘⢒⣁⣂⡙⣪⣬⡩⣐⣇⣼⣤⡇⡖⠖⢈⠌⠼⡗⢓⠥⡛⡽⢛⡍⣹⠆⠪⠛⠕⠝⡦⢙⡬⣆⣛⣜⣭';
   const charsLength = chars.length;
 
   const out: string[] = [];
   for(let i = 0; i < text.length; ++i) {
-    const char = text.charCodeAt(i);
-    out.push(chars[char % charsLength]);
+    out.push(chars[i % charsLength]);
   }
 
   return out.join('');
