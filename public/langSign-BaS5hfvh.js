1d7311bb008b209203776b6d429160eebfc0cd9d
Eduard Kuzmenko <thanwords24@gmail.com>
2025-02-12 18:48:08 +0400

    Build

---
+++ public/langSign-BaS5hfvh.js
@@ -0,0 +1,9 @@
+const e={"Login.Title":"Sign in to Telegram","Login.PhoneLabel":"Phone Number","Login.PhoneLabelInvalid":"Phone Number Invalid","Login.StartText":`Please confirm your country code
+and enter your phone number.`,"Login.Code.SentSms":`We have sent you an SMS
+with the code.`,"Login.Code.SentInApp":`We have sent you a message in Telegram
+with the code.`,"Login.Code.SentCall":`We will call you and dictate
+the code.`,"Login.Code.SentUnknown":`Please check everything
+for a code (type: %s)`,"Login.Password.Title":"Enter Your Password","Login.Password.Subtitle":`Your account is protected with
+an additional password`,"Login.Register.Subtitle":`Enter your name and add
+a profile photo`,PleaseWait:"Please wait...",Code:"Code",LoginPassword:"Password",YourName:"Your Name",FirstName:"First name (required)",LastName:"Last name (optional)",StartMessaging:"Start Messaging",Country:"Country","Contacts.PhoneNumber.Placeholder":"Phone Number","Login.Next":"Next","Login.ContinueOnLanguage":"Continue in English","Login.QR.Title":"Log in to Telegram by QR Code","Login.QR.Help1":"Open Telegram on your phone","Login.QR.Help2":"Go to **Settings** > **Devices** > **Link Desktop Device**","Login.QR.Help3":"Point your phone at this screen to confirm login","Login.QR.Cancel":"Log in by phone Number","Login.QR.Login":"Log in by QR Code","PhoneNumber.Code.Fragment.Info":"Get the code in the Anonymous Numbers section on [Fragment](%1$@)",PHONE_CODE_INVALID:"Invalid code",PHONE_CODE_EXPIRED:"Code expired",PASSWORD_HASH_INVALID:"Incorrect password"};export{e as default};
+//# sourceMappingURL=langSign-BaS5hfvh.js.map
