81d99baaf081f11044ed7dbf13216b3462ae365a
H7GhosT <nord.doogee@gmail.com>
2025-03-15 12:54:08 +0200

    Quick save video editing

--- src/components/mediaEditor/canvas/mainCanvas.tsx
+++ src/components/mediaEditor/canvas/mainCanvas.tsx
@@ -1,15 +1,15 @@
 import {onCleanup, onMount, Show} from 'solid-js';
 
-import {hexToRgb} from '../../../helpers/color';
-
 import {useMediaEditorContext} from '../context';
 
 import useFinalTransform from './useFinalTransform';
+import PreviewBrushSize from './previewBrushSize';
 import ResizableLayers from './resizableLayers';
 import RotationWheel from './rotationWheel';
 import CropHandles from './cropHandles';
 import BrushCanvas from './brushCanvas';
 import ImageCanvas from './imageCanvas';
+
 
 export default function MainCanvas() {
   let container: HTMLDivElement;
@@ -36,18 +36,7 @@ export default function MainCanvas() {
         <Show when={editorState.isReady}>
           <BrushCanvas />
           <ResizableLayers />
-          {editorState.previewBrushSize && (
-            <div
-              class="media-editor__preview-brush-size"
-              style={{
-                '--color': !['blur', 'eraser'].includes(editorState.currentBrush.brush) ?
-                  hexToRgb(editorState.currentBrush.color).join(',') :
-                  undefined,
-                'width': editorState.previewBrushSize + 'px',
-                'height': editorState.previewBrushSize + 'px'
-              }}
-            />
-          )}
+          <PreviewBrushSize />
           {/* WTF ref causes infinite loop when passed without callback */}
           <div ref={(el) => void (editorState.resizeHandlesContainer = el)} class="media-editor__resize-handles-overlay" />
           <CropHandles />
