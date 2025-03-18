7f0398c1417c679022b8cff8997961a539612646
H7GhosT <nord.doogee@gmail.com>
2025-03-18 09:48:47 +0200

    Small changes

--- src/helpers/number/nMap.ts
+++ src/helpers/number/nMap.ts
@@ -1,7 +1,7 @@
 
 /**
- * Maps value from a range [min, max] to another range [tMin, tMax]
+ * Maps value from a range [fromMin, fromMax] to another range [toMin, toMax]
  */
-export default function nMap(value: number, min: number, max: number, tMin: number, tMax: number) {
-  return ((value - min) / (max - min)) * (tMax - tMin) + tMin;
+export default function nMap(value: number, fromMin: number, fromMax: number, toMin: number, toMax: number) {
+  return ((value - fromMin) / (fromMax - fromMin)) * (toMax - toMin) + toMin;
 }
