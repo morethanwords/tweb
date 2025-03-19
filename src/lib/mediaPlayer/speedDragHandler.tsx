7cc8b491b553779c2f0a225043663448258b5ee1
Eduard Kuzmenko <thanwords24@gmail.com>
2025-03-19 13:43:39 +0400

    Fix media viewer controls color

--- src/lib/mediaPlayer/speedDragHandler.tsx
+++ src/lib/mediaPlayer/speedDragHandler.tsx
@@ -8,6 +8,7 @@ import clamp from '../../helpers/number/clamp';
 import {IS_MOBILE} from '../../environment/userAgent';
 
 import {i18n} from '../langPack';
+import classNames from '../../helpers/string/classNames';
 
 type SpeedDragHandlerProps = {
   video: HTMLVideoElement;
@@ -17,11 +18,11 @@ type SpeedDragHandlerProps = {
 
 type InternalSpeedDragHandlerProps = SpeedDragHandlerProps & {
   controlsRef: (value: SpeedDragHandlerControls) => void;
-}
+};
 
 type SpeedDragHandlerControls = {
   isChangingSpeed: () => boolean;
-}
+};
 
 
 const HIDE_TIP_MOUSE_MOVE_THRESHOLD = 2;
@@ -43,7 +44,7 @@ export function SpeedDragHandler(props: InternalSpeedDragHandlerProps) {
 
   const controls: SpeedDragHandlerControls = {
     isChangingSpeed: () => showSpeed()
-  }
+  };
   props.controlsRef(controls);
 
   let speedTipRef: HTMLDivElement;
@@ -106,7 +107,7 @@ export function SpeedDragHandler(props: InternalSpeedDragHandlerProps) {
 
       showTimeout = window.setTimeout(() => {
         show();
-      }, SHOW_TIP_TIMEOUT)
+      }, SHOW_TIP_TIMEOUT);
     });
 
     listenerSetter.add(window)('pointermove', (e) => {
@@ -204,25 +205,25 @@ export function SpeedDragHandler(props: InternalSpeedDragHandlerProps) {
   const formattedSpeed = () => currentSpeed().toFixed(1).replace(/\.0$/, '');
 
   return (
-    <Show when={showSpeed()}>
+    <Show when={showSpeed() || true}>
       <div
-        class='speed-drag-handler__speed'
+        class="speed-drag-handler__speed"
         style={{
           '--animation-duration-multiplier': currentSpeed()
         }}
       >
-        <div class='speed-drag-handler__speed-number'>
+        <div class="speed-drag-handler__speed-number">
           {formattedSpeed()}x
         </div>
-        <div class='speed-drag-handler__speed-arrows'>
+        <div class="speed-drag-handler__speed-arrows">
           <SpeedArrowRight />
-          <SpeedArrowRight />
+          <SpeedArrowRight class="speed-drag-handler__speed-arrow-right-last" />
         </div>
       </div>
 
       <div
         ref={speedTipRef}
-        class='speed-drag-handler__speed-tip'
+        class="speed-drag-handler__speed-tip"
         classList={{
           'speed-drag-handler__speed-tip--visible': !hideSpeedTip() && !IS_MOBILE
         }}
@@ -231,11 +232,11 @@ export function SpeedDragHandler(props: InternalSpeedDragHandlerProps) {
           '--top': mouseCoords()[1] + 'px'
         }}
       >
-        <div class='speed-drag-handler__speed-tip-arrows'>
-          <IconTsx icon='arrow_prev' />
-          <IconTsx icon='arrow_next' />
+        <div class="speed-drag-handler__speed-tip-arrows">
+          <IconTsx icon="arrow_prev" />
+          <IconTsx icon="arrow_next" />
         </div>
-        <div class='speed-drag-handler__speed-tip-description'>
+        <div class="speed-drag-handler__speed-tip-description">
           {i18n('PlaybackRateDragTip')}
         </div>
       </div>
@@ -273,10 +274,10 @@ function areSpeedsEqual(a: number, b: number) {
   return a.toFixed(1) === b.toFixed(1);
 }
 
-function SpeedArrowRight() {
+function SpeedArrowRight(props: {class?: string}) {
   return (
-    <svg class='speed-drag-handler__speed-arrow-right' width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class={classNames('speed-drag-handler__speed-arrow-right', props.class)} width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path opacity="0.7" d="M0 1.5V9.5C0 10.324 0.940764 10.7944 1.6 10.3L6.93333 6.3C7.46667 5.9 7.46667 5.1 6.93333 4.7L1.6 0.7C0.940764 0.205573 0 0.675955 0 1.5Z" fill="currentColor"/>
     </svg>
-  )
+  );
 }
