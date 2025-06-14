726401d8b88b0a22d6406845c55faccb80431113
H7GhosT <h7ghost-on-github.silenced089@passmail.com>
2025-06-14 21:32:41 +0300

    Thumbnail track and something more

---
+++ src/helpers/useSwipe.ts
@@ -0,0 +1,64 @@
+import {StandardLonghandPropertiesHyphen} from 'csstype';
+import {Accessor, onCleanup} from 'solid-js';
+
+import ListenerSetter from './listenerSetter';
+
+export type SwipeDirectiveArgs = {
+  globalCursor?: StandardLonghandPropertiesHyphen['cursor'];
+  onStart?: (e: PointerEvent) => void;
+  onMove?: (xDiff: number, yDiff: number, e: PointerEvent) => void;
+  onEnd?: (xDiff: number, yDiff: number, e: PointerEvent) => void;
+};
+
+declare module 'solid-js' {
+  namespace JSX {
+    interface Directives {
+      swipe: SwipeDirectiveArgs;
+    }
+  }
+}
+
+export default function swipe(element: HTMLElement, args: Accessor<SwipeDirectiveArgs>) {
+  let isDragging = false;
+  let initialX: number, initialY: number;
+
+  const listenerSetter = new ListenerSetter;
+
+  const getDiff = (e: PointerEvent) => [
+    e.clientX - initialX,
+    e.clientY - initialY
+  ] as const;
+
+  element.addEventListener('pointerdown', (e) => {
+    isDragging = true;
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    const {onStart, globalCursor} = args();
+    onStart?.(e);
+
+    globalCursor && document.body.style.setProperty('cursor', globalCursor, 'important');
+  });
+
+  listenerSetter.add(document)('pointermove', (e) => {
+    if(!isDragging) return;
+
+    const {onMove} = args();
+    onMove?.(...getDiff(e), e);
+  });
+
+  listenerSetter.add(document)('pointerup', (e) => {
+    if(!isDragging) return;
+
+    const {onEnd, globalCursor} = args();
+    onEnd?.(...getDiff(e), e);
+    globalCursor && document.body.style.removeProperty('cursor');
+
+    isDragging = false;
+    initialX = initialY = undefined;
+  });
+
+  onCleanup(() => {
+    listenerSetter.removeAll();
+  });
+}
