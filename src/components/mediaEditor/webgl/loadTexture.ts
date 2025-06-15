df63dd8839b5e3f9a01358a59e612a874ba773a4
H7GhosT <h7ghost-on-github.silenced089@passmail.com>
2025-06-15 18:41:39 +0300

    Make texture magnifying linear

--- src/components/mediaEditor/webgl/loadTexture.ts
+++ src/components/mediaEditor/webgl/loadTexture.ts
@@ -17,7 +17,7 @@ export function loadTexture(gl: WebGLRenderingContext, url: string) {
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
       resolve({texture, image});
     });
