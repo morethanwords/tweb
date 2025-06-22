f2795ae686a5ff679bd8eff259c5b88d8b8f1c92
H7GhosT <h7ghost-on-github.silenced089@passmail.com>
2025-06-22 19:06:21 +0300

    Move video playback init to separate file

---
+++ src/components/mediaEditor/canvas/initVideoPlayback.ts
@@ -0,0 +1,93 @@
+import {createEffect, onCleanup} from 'solid-js';
+import {useMediaEditorContext} from '../context';
+import {animate} from '../../../helpers/animation';
+import clamp from '../../../helpers/number/clamp';
+
+
+type Args = {
+  gl: WebGLRenderingContext;
+  drawAdjustedImage: () => void;
+};
+
+export default function initVideoPlayback({gl, drawAdjustedImage}: Args) {
+  const {editorState, actions} = useMediaEditorContext();
+  const {renderingPayload} = editorState;
+  const video = renderingPayload.media.video;
+
+  if(!video) return;
+
+  // We don't want this 'seeked' event to be fired when generating the final result
+  let pendingSeek = false;
+
+  actions.setVideoTime = (time: number, redraw = true) => {
+    editorState.currentVideoTime = time;
+    video.currentTime = time * video.duration;
+    if(redraw) pendingSeek = true;
+  };
+
+  const seekListener = () => {
+    if(!pendingSeek) return;
+    pendingSeek = false;
+
+    gl.bindTexture(gl.TEXTURE_2D, renderingPayload.texture);
+    gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, video);
+
+    drawAdjustedImage();
+  };
+
+  video.addEventListener('seeked', seekListener);
+  onCleanup(() => {
+    video.removeEventListener('seeked', seekListener);
+  });
+
+  createEffect(() => {
+    if(!editorState.isPlaying) return;
+
+    let
+      frameCallbackId: number,
+      playing = true,
+      skip = 0
+    ;
+
+    function frameCallback() {
+      gl.bindTexture(gl.TEXTURE_2D, renderingPayload.texture);
+      gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, video);
+
+      drawAdjustedImage();
+
+      frameCallbackId = video.requestVideoFrameCallback(() => frameCallback());
+    }
+
+    const timeout = self.setTimeout(() => {
+      if(editorState.currentVideoTime >= editorState.videoCropStart + editorState.videoCropLength)
+        actions.setVideoTime(editorState.videoCropStart);
+
+      frameCallbackId = video.requestVideoFrameCallback(frameCallback);
+      video.play();
+
+      animate(() => {
+        skip = (skip + 1) % 3;
+
+        if(skip) return true;
+        if(!playing) return false;
+
+        editorState.currentVideoTime = video.currentTime / video.duration || 0;
+
+        if(video.ended || video.paused || editorState.currentVideoTime >= editorState.videoCropStart + editorState.videoCropLength) {
+          playing = false;
+          editorState.isPlaying = false;
+          editorState.currentVideoTime = clamp(editorState.currentVideoTime, editorState.videoCropStart, editorState.videoCropStart + editorState.videoCropLength);
+        }
+
+        return playing;
+      });
+    }, 200); // Let the pause/play icon animation finish as it might lag without this
+
+    onCleanup(() => {
+      playing = false;
+      self.clearTimeout(timeout);
+      video.cancelVideoFrameCallback(frameCallbackId);
+      video.pause();
+    });
+  });
+};
