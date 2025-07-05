116651a926509636b635f08d176b317623faddc5
H7GhosT <h7ghost-on-github.silenced089@passmail.com>
2025-07-05 16:38:41 +0300

    Define solid element

---
+++ src/lib/solidjs/defineSolidElement.ts
@@ -0,0 +1,219 @@
+import {createEffect, createRoot, JSX} from 'solid-js';
+import {createMutable, unwrap} from 'solid-js/store';
+import {render} from 'solid-js/web';
+
+
+type Args<ObservedAttribute extends string, Props, Controls extends Object = {}> = {
+  name: string;
+  component: CustomElementComponent<ObservedAttribute, Props, Controls>;
+  observedAttributes?: ObservedAttribute[];
+  shadow?: boolean;
+}
+
+export type PassedProps<Props extends Object = {}> = Props & {
+  readonly element: HTMLElement;
+};
+
+type AttributesRecord<ObservedAttribute extends string> = Record<ObservedAttribute, string | undefined | null>;
+type MutableStore<T extends Object> = T;
+
+type CustomElementComponent<ObservedAttribute extends string, Props, Controls = {}> =
+  (props: PassedProps<MutableStore<Props>>, attributes: MutableStore<AttributesRecord<ObservedAttribute>>, controls: Controls) => JSX.Element;
+
+export type DefinedSolidElement<ObservedAttribute extends string = string, Props extends Object = {}, Controls = {}> = ReturnType<typeof defineSolidElement<Props, ObservedAttribute, Controls>>;
+
+/**
+ * Defines an HMR capable custom element with solid-js rendering
+ *
+ * Don't forget to add `if(import.meta.hot) import.meta.hot.accept();` in the file you're
+ * declaring the component to take advantage of Hot Module Replacement
+ *
+ * Note that it might still need a refresh if you change other parameters except the component logic
+ *
+ * @example
+ * const MyElement = defineSolidElement({
+ *   name: 'my-element',
+ *   observedAttributes: ['size', 'something-else'],
+ *   // props and attributes are reactive mutable stores, meaning you can also mutate them directly
+ *   component: (props: {fancyProp: Date}, attributes, controls: {someAction: () => void}) => {
+ *     // component logic in here
+ *
+ *     // expose some methods to the element
+ *     controls.someAction = () => setInternalSignal('to something');
+ *
+ *     return <>...</>;
+ *   }
+ * });
+ *
+ * const el = new MyElement;
+ * el.feedProps({fancyProp: new Date});
+ *
+ * // Append to DOM after feeding the props, unless it relies solely on attributes, or handles undefined props
+ * otherElement.append(el);
+ *
+ * // Can use attributes if you want
+ * el.setAttribute('size', '400');
+ *
+ * // Use internal methods through the `controls` property
+ * el.controls.someAction();
+ */
+export default function defineSolidElement<Props extends Object, ObservedAttribute extends string, Controls = {}>({
+  name,
+  component,
+  observedAttributes = [],
+  shadow = false
+}: Args<ObservedAttribute, Props, Controls>) {
+  //
+  // When the module is hot replaced
+  if(customElements.get(name)) {
+    const previousElementClass = customElements.get(name) as typeof SolidElement;
+    previousElementClass.swapComponentFromHMR(component);
+    return previousElementClass;
+  }
+
+  let instances: (InstanceType<typeof SolidElement>)[];
+
+  if(import.meta.hot) {
+    instances = [];
+  }
+
+  const className = burgerToPascal(name);
+
+  const SolidElement = class extends HTMLElement {
+    private mountPoint: ShadowRoot | this;
+
+    private attributesStore: MutableStore<AttributesRecord<ObservedAttribute>>;
+    private propsStore: PassedProps<Props>;
+
+    private disposeContent?: () => void;
+    private disposeStores?: () => void;
+
+    /**
+     * Persist props when the component gets mounted / unmounted and allow initialization before the element
+     * is added to the DOM
+     */
+    private savedProps = ((self: typeof this) => ({
+      get element() {
+        return self;
+      }
+    } as unknown as PassedProps<Props>))(this);
+
+    public readonly controls = {} as Controls;
+
+    /**
+     * For HMR
+     */
+    public static Component = component;
+
+    public static swapComponentFromHMR(newComponent: CustomElementComponent<ObservedAttribute, Props, Controls>) {
+      if(import.meta.hot) {
+        SolidElement.Component = newComponent;
+        instances.forEach((instance) => {
+          instance?.mount?.();
+        });
+      }
+    }
+
+    static get observedAttributes() {
+      return observedAttributes;
+    }
+
+    constructor() {
+      super();
+
+      this.mountPoint = shadow ? this.attachShadow({mode: 'open'}) : this;
+    }
+
+    connectedCallback() {
+      this.mount();
+
+      if(import.meta.hot) {
+        instances.push(this);
+      }
+    }
+
+    disconnectedCallback() {
+      this.unmount();
+
+      if(import.meta.hot) {
+        const idx = instances.indexOf(this);
+        if(idx > -1) instances.splice(idx, 1);
+      }
+    }
+
+    attributeChangedCallback(name: ObservedAttribute, _oldValue: string, newValue: string) {
+      this.attributesStore[name] = newValue; // Let's hope this will not trigger infinite loops, as the values are compared before updates
+    }
+
+    public feedProps<Full extends boolean = true>(props: Full extends true ? Props : Partial<Props>) {
+      if(this.disposeStores) {
+        Object.assign(this.propsStore, props);
+      } else {
+        Object.assign(this.savedProps, props);
+      }
+    }
+
+    private initStores() {
+      createRoot(dispose => {
+        this.disposeStores = dispose;
+
+        this.propsStore = createMutable(this.savedProps);
+        this.attributesStore = createMutable({} as AttributesRecord<ObservedAttribute>);
+
+        createEffect(() => Object.keys(this.attributesStore).forEach(key => {
+          const attributeName = key as ObservedAttribute;
+          const value = this.attributesStore[attributeName];
+
+          if(this.getAttribute(attributeName) === this.attributesStore[attributeName]) return;
+
+          if(value === null || value === undefined) {
+            this.removeAttribute(attributeName);
+          } else {
+            this.setAttribute(attributeName, value);
+          }
+        }));
+      });
+    }
+
+    private mount() {
+      let savedAttributes: AttributesRecord<ObservedAttribute>;
+
+      // can happen only on hmr
+      if(this.disposeStores) savedAttributes = unwrap(this.attributesStore);
+
+      this.unmount();
+      const ComponentToMount = SolidElement.Component;
+
+      this.initStores();
+      if(savedAttributes) Object.assign(this.attributesStore, savedAttributes);
+
+      this.disposeContent = createRoot(dispose => {
+        render(() => ComponentToMount(this.propsStore, this.attributesStore, this.controls), this.mountPoint);
+        return dispose;
+      });
+    }
+
+    private unmount() {
+      this.mountPoint.replaceChildren(); // Don't leave trash in there
+
+      this.disposeContent?.();
+      this.disposeStores?.();
+      this.disposeStores = this.disposeContent = undefined;
+    }
+
+    get [Symbol.toStringTag]() {
+      return className;
+    }
+  };
+
+  customElements.define(name, SolidElement);
+
+  return SolidElement;
+}
+
+function burgerToPascal(str: string) {
+  return str
+  .split('-')
+  .map(part => part.charAt(0).toUpperCase() + part.slice(1))
+  .join('');
+}
