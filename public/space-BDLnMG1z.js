5e3c4b5e72ba9201e0cef8ed57ab9613d0486601
Eduard Kuzmenko <thanwords24@gmail.com>
2025-07-08 18:19:06 -0400

    Build

---
+++ public/space-BDLnMG1z.js
@@ -0,0 +1,2 @@
+import{cC as e,t as n,bb as i,Z as o}from"./index-Biwfcedj.js";var p=n("<div>");function l(s){const[t,a]=e(s,["amount","withTransition"]);return(()=>{var r=p();return i(r,o(a,{get style(){return{"padding-top":t.amount,transition:t.withTransition?".2s":void 0}}}),!1,!1),r})()}export{l as S};
+//# sourceMappingURL=space-BDLnMG1z.js.map
