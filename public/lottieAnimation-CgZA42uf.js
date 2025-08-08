77272c05d148cd4e1d54622a9e5d03206b933afe
Eduard Kuzmenko <thanwords24@gmail.com>
2025-08-08 18:57:54 +0400

    Build

--- public/lottieAnimation-CgZA42uf.js
+++
@@ -1,2 +0,0 @@
-import{dc as n,b as a,Z as s}from"./index-CWTkMyla.js";import{c as r}from"./classNames-CW2brDtv.js";import{v as m,L as c}from"./appDialogsManager-BhLqZLHU.js";const d="_LottieAnimation_r3b7k_1",l="_AdditionalPadding_r3b7k_10",L={LottieAnimation:d,AdditionalPadding:l},_=t=>{const{lottieLoader:o}=n(),i=m();return a(c,s({lottieLoader:o,onPromise:e=>i.collect(e),get class(){return r(t.class,L.LottieAnimation)},restartOnClick:!0},t))};export{_ as L,L as c};
-//# sourceMappingURL=lottieAnimation-CgZA42uf.js.map
