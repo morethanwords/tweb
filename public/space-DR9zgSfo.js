1832485e4c0fe320cc61314be0c5e18534589017
Eduard Kuzmenko <thanwords24@gmail.com>
2025-09-09 19:38:19 +0400

    Build

---
+++ public/space-DR9zgSfo.js
@@ -0,0 +1,2 @@
+import{cC as e,t as n,bf as i,Z as o}from"./index-DZP9jKOo.js";var p=n("<div>");function l(s){const[t,a]=e(s,["amount","withTransition"]);return(()=>{var r=p();return i(r,o(a,{get style(){return{"padding-top":t.amount,transition:t.withTransition?".2s":void 0}}}),!1,!1),r})()}export{l as S};
+//# sourceMappingURL=space-DR9zgSfo.js.map
