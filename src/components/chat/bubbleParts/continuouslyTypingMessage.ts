b99ad39b188b8fe5f63ee778c79f436d54227b09
H7GhosT <h7ghost-on-github.silenced089@passmail.com>
2025-10-28 13:40:58 +0200

    Refactor continously typing message and add 3 dots

--- src/components/chat/bubbleParts/continuouslyTypingMessage.ts
+++ src/components/chat/bubbleParts/continuouslyTypingMessage.ts
@@ -4,125 +4,42 @@ import styles from './continuouslyTypingMessage.module.scss';
 
 type WrapContinuouslyTypingMessageArgs = {
   root: Node;
+  isEnd?: boolean;
   prevPosition?: number;
 };
 
-const TARGET_TIME_TO_WRITE = 1500;
+type Result = {
+  allNodes: Node[];
+  clean: () => void;
+  currentPosition: number;
+  currentNodeIdx: number;
+};
 
-const BASE_DELAY = 60 * 1_000 / (2_000 * 5); // 2_000wpm
-const DELAY_VARIATION = 0.3;
+export function wrapContinuouslyTypingMessage({root, isEnd = false, prevPosition = -1}: WrapContinuouslyTypingMessageArgs) {
+  const maxPosition = getMaxPosition(root);
 
-const SCROLL_VIEW_DELAY = 200;
-
-// Try to write it with the base speed of 2000wpm or burst it in 3 seconds if it's a long message
-function getRandomDelay(targetDelay: number) {
-  const delay = Math.max(1 /* ms */, Math.min(BASE_DELAY, targetDelay));
-  return delay + Math.random() * delay * DELAY_VARIATION;
-}
-
-export function wrapContinuouslyTypingMessage({root, prevPosition = -1}: WrapContinuouslyTypingMessageArgs) {
-  const initialTreeWalker = document.createTreeWalker(root, NodeFilter.SHOW_ALL);
-
-  const initialNodes: Node[] = [];
-
-  while(initialTreeWalker.nextNode()) {
-    initialNodes.push(initialTreeWalker.currentNode);
-  }
-
-  let currentPositionMax = -1;
-
-  for(const node of initialNodes) {
-    if(node.nodeType === Node.TEXT_NODE) {
-      const chars = node.textContent.split('').map(char => {
-        const span = document.createElement('span');
-        span.textContent = char;
-        currentPositionMax++;
-        return span;
-      });
-      const fragment = document.createDocumentFragment();
-      fragment.append(...chars);
-      node.parentNode?.replaceChild(fragment, node);
-    }
-  }
-
-  let currentNodeIdx = 0;
-
-  const allNodesTreeWalker = document.createTreeWalker(root, NodeFilter.SHOW_ALL);
-
-  const allNodes: Node[] = [];
-
-
-  {
-    let position = 0;
-
-    while(allNodesTreeWalker.nextNode()) {
-      const node = allNodesTreeWalker.currentNode;
-      allNodes.push(node);
-
-      if(node.nodeType === Node.TEXT_NODE) {
-        position += node.textContent.length;
-      } else if(node instanceof Element && position > prevPosition) {
-        node.classList.add(styles.hidden);
-      }
-
-      if(position <= prevPosition) {
-        currentNodeIdx++;
-      }
-    }
-  }
+  const {allNodes, currentNodeIdx} = hidePrevElements(root, prevPosition);
 
   let
-    nextTime = performance.now(),
-    nextScrollTime = performance.now() + SCROLL_VIEW_DELAY,
     lastElement: Element,
-    cleaned = false
+    lastTextNode: Node,
+    cleaned = false,
+    ended = false
   ;
 
   function clean() {
     cleaned = true;
   };
 
-  function typeNext() {
-    result.currentPosition++;
+  function onEnd() {
+    if(ended) return;
+    ended = true;
 
-    let stillHere = true;
-    for(; result.currentNodeIdx < allNodes.length && stillHere; result.currentNodeIdx++) {
-      const node = allNodes[result.currentNodeIdx];
-
-      if(node instanceof Element) {
-        lastElement = node;
-        node.classList.remove(styles.hidden);
-      }
-
-      if(node.nodeType === Node.TEXT_NODE && node.textContent.length) {
-        stillHere = false;
-      }
-    }
-
-    if(result.currentNodeIdx >= allNodes.length) {
-      clean();
-    }
+    if(!isEnd) appendDots(lastTextNode);
+    animate(() => {
+      lastElement?.scrollIntoView({behavior: 'smooth', block: 'center'});
+    });
   }
-
-  const targetDelay = TARGET_TIME_TO_WRITE / (currentPositionMax - prevPosition);
-
-  animate(() => {
-    if(cleaned) return false;
-
-    const now = performance.now();
-
-    while(now > nextTime) {
-      typeNext();
-      nextTime = nextTime + getRandomDelay(targetDelay);
-    }
-
-    if(now > nextScrollTime) {
-      lastElement.scrollIntoView({behavior: 'smooth', block: 'center'});
-      nextScrollTime = now + SCROLL_VIEW_DELAY;
-    }
-
-    return true;
-  });
 
   const result = {
     allNodes,
@@ -131,5 +48,179 @@ export function wrapContinuouslyTypingMessage({root, prevPosition = -1}: WrapCon
     currentNodeIdx
   };
 
+  runAnimation({
+    typeNext: () => typeNext({
+      result,
+      setLastElement: (element) => lastElement = element,
+      setLastTextNode: (node) => lastTextNode = node,
+      onEnd
+    }),
+    getLastElement: () => lastElement,
+    isCleaned: () => cleaned,
+    maxPosition,
+    prevPosition
+  });
+
   return result;
+}
+
+
+function getMaxPosition(root: Node) {
+  const initialTreeWalker = document.createTreeWalker(root, NodeFilter.SHOW_ALL);
+
+  const initialNodes: Node[] = [];
+
+  while(initialTreeWalker.nextNode()) {
+    initialNodes.push(initialTreeWalker.currentNode);
+  }
+
+  let position = -1;
+
+  for(const node of initialNodes) {
+    if(node.nodeType === Node.TEXT_NODE) {
+      const chars = node.textContent.split('').map(char => {
+        const span = document.createElement('span');
+        span.textContent = char;
+        position++;
+        return span;
+      });
+      const fragment = document.createDocumentFragment();
+      fragment.append(...chars);
+      node.parentNode?.replaceChild(fragment, node);
+    }
+  }
+
+  return position;
+}
+
+
+function hidePrevElements(root: Node, toPosition: number) {
+  let currentNodeIdx = 0;
+
+  const allNodesTreeWalker = document.createTreeWalker(root, NodeFilter.SHOW_ALL);
+  const allNodes: Node[] = [];
+
+  let position = 0;
+
+  while(allNodesTreeWalker.nextNode()) {
+    const node = allNodesTreeWalker.currentNode;
+    allNodes.push(node);
+
+    if(node.nodeType === Node.TEXT_NODE) {
+      position += node.textContent.length;
+    } else if(node instanceof Element && position > toPosition) {
+      node.classList.add(styles.hidden);
+    }
+
+    if(position <= toPosition) {
+      currentNodeIdx++;
+    }
+  }
+
+  return {
+    allNodes,
+    currentNodeIdx
+  };
+}
+
+
+function appendDots(node: Node) {
+  const parent = node.parentNode;
+  if(!(parent instanceof Element)) return;
+
+  const dots = document.createElement('span');
+  dots.className = styles.Dots;
+  dots.textContent = ' ';
+
+  new Array(3).fill(null).forEach((_, idx) => {
+    const dot = document.createElement('span');
+    dot.textContent = '.';
+    dot.classList.add(styles.Dot, styles['Dot' + (idx + 1)]);
+    dots.appendChild(dot);
+  });
+
+  parent.appendChild(dots);
+}
+
+
+type TypeNextArgs = {
+  result: Result;
+  setLastElement: (element: Element) => void;
+  setLastTextNode: (node: Node) => void;
+  onEnd: () => void;
+};
+
+function typeNext({result, setLastElement, setLastTextNode, onEnd}: TypeNextArgs) {
+  const {allNodes, clean} = result;
+  result.currentPosition++;
+
+  let stillHere = true;
+  for(; result.currentNodeIdx < allNodes.length && stillHere; result.currentNodeIdx++) {
+    const node = allNodes[result.currentNodeIdx];
+
+    if(node instanceof Element) {
+      setLastElement(node);
+      node.classList.remove(styles.hidden);
+    }
+
+    if(node.nodeType === Node.TEXT_NODE && node.textContent.length) {
+      setLastTextNode(node);
+      stillHere = false;
+    }
+  }
+
+  if(result.currentNodeIdx >= allNodes.length) {
+    clean();
+    onEnd();
+  }
+}
+
+
+const BASE_DELAY = 60 * 1_000 / (800 * 5); // 800wpm
+const DELAY_VARIATION = 0.3;
+
+// Try to write it with the base speed of 800wpm or burst it in 1.5 seconds if it's a long message
+function getRandomDelay(targetDelay: number) {
+  const delay = Math.max(1 /* ms */, Math.min(BASE_DELAY, targetDelay));
+  return delay + Math.random() * delay * DELAY_VARIATION;
+}
+
+
+const TARGET_TIME_TO_WRITE = 1500;
+// const TARGET_TIME_TO_WRITE = 1000;
+const SCROLL_VIEW_DELAY = 200;
+
+type RunAnimationArgs = {
+  typeNext: () => void;
+  isCleaned: () => boolean;
+  getLastElement: () => Element;
+  maxPosition: number;
+  prevPosition: number;
+};
+
+function runAnimation({typeNext, isCleaned, getLastElement, maxPosition, prevPosition}: RunAnimationArgs) {
+  const targetDelay = TARGET_TIME_TO_WRITE / (maxPosition - prevPosition);
+
+  let
+    nextTime = performance.now(),
+    nextScrollTime = performance.now() + SCROLL_VIEW_DELAY
+  ;
+
+  animate(() => {
+    if(isCleaned()) return false;
+
+    const now = performance.now();
+
+    while(now > nextTime && !isCleaned()) {
+      typeNext();
+      nextTime = nextTime + getRandomDelay(targetDelay);
+    }
+
+    if(now > nextScrollTime) {
+      getLastElement().scrollIntoView({behavior: 'smooth', block: 'center'});
+      nextScrollTime = now + SCROLL_VIEW_DELAY;
+    }
+
+    return true;
+  });
 }
