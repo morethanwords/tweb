30c7a6e767df41ddd1b4d92e8cdd46f270eceaec
oladeji <oladeji@ids.company>
2025-11-12 12:42:59 +0100

    rebuild

---
+++ public/useAppConfig-Do5jbtij.js
@@ -0,0 +1,2 @@
+import{cH as s,ad as t}from"./index-ooHDKuRB.js";import{E as a}from"./appDialogsManager-C6LyW2FC.js";const n=()=>{const{rootScope:e}=s(),r=a();return t(()=>{const o=e.managers.apiManager.getAppConfig();return r.collect(o),o})};export{n as u};
+//# sourceMappingURL=useAppConfig-Do5jbtij.js.map
