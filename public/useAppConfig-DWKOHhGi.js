2071f286df0737b223da54001a501dd904c47117
Eduard Kuzmenko <thanwords24@gmail.com>
2025-11-25 18:44:03 +0400

    Build

---
+++ public/useAppConfig-DWKOHhGi.js
@@ -0,0 +1,2 @@
+import{dQ as s,ad as t}from"./index-bLPCEswf.js";import{E as a}from"./appDialogsManager-CNd4t2iE.js";const n=()=>{const{rootScope:e}=s(),r=a();return t(()=>{const o=e.managers.apiManager.getAppConfig();return r.collect(o),o})};export{n as u};
+//# sourceMappingURL=useAppConfig-DWKOHhGi.js.map
