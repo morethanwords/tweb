0e5c8cd7802c4119d1b60195ec706fc89205fbea
alina sireneva <alina@tei.su>
2025-12-07 17:53:59 +0300

    fix: lazy prono store

--- src/stores/promo.ts
+++ src/stores/promo.ts
@@ -5,12 +5,16 @@ import {createStore, reconcile} from 'solid-js/store';
 
 const [promoData, setPromoData] = createStore<MyPromoData>({pendingSuggestions: []});
 
-rootScope.managers.appPromoManager.getPromoData()
-
-rootScope.addEventListener('promo_data_update', (data) => {
-  setPromoData(reconcile(data));
-});
-
+let _loaded = false
 export function usePendingSuggestions() {
+  if(!_loaded) {
+    _loaded = true;
+    rootScope.managers.appPromoManager.getPromoData()
+
+    rootScope.addEventListener('promo_data_update', (data) => {
+      setPromoData(reconcile(data));
+    });
+  }
+
   return createMemo(() => new Set(promoData.pendingSuggestions));
 }
