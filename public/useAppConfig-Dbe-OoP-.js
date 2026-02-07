01176f1698d6bd07dfbebf068a44a1cc26dba67b
Eduard Kuzmenko <thanwords24@gmail.com>
2026-02-07 18:11:03 +0400

    Build

---
+++ public/useAppConfig-Dbe-OoP-.js
@@ -0,0 +1,2 @@
+import{dR as s,af as t}from"./index-DbIWMua-.js";import{a7 as a}from"./appDialogsManager-Cr5EkjPL.js";const n=()=>{const{rootScope:e}=s(),r=a();return t(()=>{const o=e.managers.apiManager.getAppConfig();return r.collect(o),o})};export{n as u};
+//# sourceMappingURL=useAppConfig-Dbe-OoP-.js.map
---
+++ public/useIsCleaned-DfjCXAyj.js
@@ -0,0 +1,2 @@
+import{a7 as a}from"./index-DbIWMua-.js";function r(){let e=!1;return a(()=>{e=!0}),()=>e}export{r as u};
+//# sourceMappingURL=useIsCleaned-DfjCXAyj.js.map
